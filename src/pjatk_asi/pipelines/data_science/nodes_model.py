784f7132e02801061b241223277748d8f2e0b30e
Adrian <adi.jobda@gmail.com>
2024-04-12 11:29:23 +0200

    fix logger error and add data files to gitignore
    Uruchamianie kedro wywalało mi błędy na loggerze, dodałem format konkatenacji starego pythona i zadziałało, z tego co wiem to zależy od wersji paczek. Niemniej wrzuciłem legacy format więc ten powinien działać wszystkim

--- src/pjatk_asi/pipelines/data_science/nodes_model.py
+++ src/pjatk_asi/pipelines/data_science/nodes_model.py
@@ -12,23 +12,18 @@ def select_hiperparameters(X: pd.DataFrame, Y: pd.DataFrame) -> GradientBoosting
     pipeline = Pipeline([
         ('clf', GradientBoostingClassifier())
     ])
-
     # Define parameter grid for GridSearchCV
     param_grid = {
         'clf__n_estimators': [50, 100, 150, 200],
         'clf__learning_rate': [0.01, 0.1, 0.2],
         'clf__max_depth': [3, 4, 5, 6]
     }
-
     # Create GridSearchCV object
     grid_search = GridSearchCV(pipeline, param_grid, cv=10, scoring='roc_auc', n_jobs=-1)
-
     grid_search.fit(X, Y)
-
     best_params = {k.replace('clf__', ''): v for k, v in grid_search.best_params_.items()}
-
     logger = logging.getLogger(__name__)
-    logger.info("Model has best parameters :", str(best_params))
+    logger.info("Model has best parameters: %s", str(best_params))
 
     return _train_final_model(X, Y, best_params)
 
@@ -51,6 +46,6 @@ def evaluate_model(X: pd.DataFrame, Y: pd.DataFrame, model: GradientBoostingClas
     Y_pred = model.predict(X)
     metrics = _get_metrics(Y, Y_pred)
     logger = logging.getLogger(__name__)
-    logger.info("Final model score :", str(metrics))
+    logger.info("Final model score: %s", str(metrics))
 
     return Y_pred
