930dd0a5b87b1d3749973a920513635710606c15
Jaroslaw Malucha <jaroslaw.malucha@gmail.com>
2024-04-19 23:01:42 +0200

    extend conda install script, small notebook tweak for windows

--- install_conda_packages.ps1
+++ install_conda_packages.ps1
@@ -10,8 +10,35 @@ conda info --envs
 conda info # make sure you have appropriate active environment
 conda list
 
-conda install python=3.10 conda pip pandas --update-all --yes
-conda install -c conda-forge scikit-learn --update-all --yes
+# conda install -n $new_py_env python=3.10 conda pip pandas --update-all --yes
+# conda install -n $new_py_env -c conda-forge scikit-learn --update-all --yes
+# pip install opendatasets
+# conda install -n $new_py_env -c conda-forge imbalanced-learn --update-all --yes
+# conda install -n $new_py_env ipykernel --update-deps --force-reinstall --yes
+
+
+
+
+conda install -n $new_py_env --yes `
+    python=3.10 `
+    conda `
+    pip `
+    pandas `
+    geopandas `
+    seaborn `
+    matplotlib `
+    ipykernel
+    
+
+
+
+conda install -n $new_py_env --yes -c conda-forge  --update-all `
+    contextily `
+    imbalanced-learn `
+    scikit-learn `
+    opendatasets
+
 python -c "import sklearn; sklearn.show_versions()"
-pip install opendatasets
-conda install -c conda-forge imbalanced-learn --update-all --yes
+
+
+# conda remove --name $new_py_env --all --yes
